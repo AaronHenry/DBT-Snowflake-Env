5c4b24918ade3874c822ba6685f9653c04b388be
Aaron Paul Henry <aaronpaulhenry@protonmail.com>
2021-12-22 00:52:46 +0000

    full name support

--- models/new_table.sql
+++ models/new_table.sql
@@ -1,1 +1,1 @@
-SELECT first_name, last_name, address, phone FROM "NORTHWINDS"."PAGILA_MART_PUBLIC"."DIMCUSTOMER"
+SELECT first_name || ' ' || last_name as full_name, address, phone FROM "NORTHWINDS"."PAGILA_MART_PUBLIC"."DIMCUSTOMER"
